1bf8d2cb9ec45381471797786f09c097051dd274
Ronel Mayor <ronelmayor0428@gmail.com>
2025-12-15 23:27:32 +0800

    init: Initial files and folder structure with pnpm-workspace, tsconfigs, package.json, eslint config, for ecommerce monorepo - apps>admin, apps>marketplace, packages>core, packages>db, packages>ui

---
+++ eslint.config.mjs
@@ -0,0 +1,106 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettier from "eslint-config-prettier";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Collect all tsconfig.json files acrross apps and packages
+const tsconfigPaths = [];
+["apps", "packages"].forEach((dir) => {
+  const baseDir = path.join(__dirname, dir);
+  if (fs.existsSync(baseDir)) {
+    for (const folder of fs.readdirSync(baseDir)) {
+      const tsconfigPath = path.join(baseDir, folder, "tsconfig.json");
+      if (fs.existsSync(tsconfigPath)) {
+        tsconfigPaths.push(tsconfigPath);
+      }
+    }
+  }
+});
+
+export default defineConfig([
+  globalIgnores([
+    "**/node_modules/**",
+    "**/.next/**",
+    "**/dist/**",
+    "**/build/**",
+    "**/out/**",
+    "**/coverage/**",
+
+    "**/.turbo/**",
+    "**/.cache/**",
+    "**/.eslintcache",
+    "**/tsconfig.tsbuildinfo",
+    "**/.env",
+    "**/.env.*",
+    "**/*.log",
+    "**/next-env.d.ts",
+
+    "packages/db/src/generated/**", // prisma
+  ]),
+
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+      next: {
+        rootDir: ["apps/*/"],
+      },
+      // explicitly added to handle monorepo path aliases for Typescript accross apps/packages
+      "import/resolver": {
+        typescript: {
+          project: tsconfigPaths,
+        },
+      },
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports" },
+      ],
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      eqeqeq: ["error", "always"],
+      curly: ["error", "all"],
+      "no-throw-literal": "error",
+
+      "next/next/no-html-link-for-pages": "off", // for next pages
+
+      // eslint-plugin-import - dependency of eslint-config-next
+      "import/order": [
+        "error",
+        {
+          groups: [
+            ["builtin", "external"],
+            ["internal", "parent", "sibling"],
+            "type",
+            ["index", "object"],
+          ],
+          pathGroups: [{ pattern: "@ecommerce/**", group: "internal" }],
+          alphabetize: { order: "asc", caseInsensitive: true },
+          sortTypesGroup: true,
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+
+  // prettier last in config to avoid conflict versus eslint setup
+  prettier,
+]);
