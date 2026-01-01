c4a0f6bcec94d63e24e1504ee96401fd4e095c63
Ronel Mayor <ronelmayor0428@gmail.com>
2026-01-01 22:28:37 +0800

    feat: Created common ui dialog in packages/ui

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,3 +1,15 @@
 export { Button } from './button';
 export { ThemeProvider } from './theme-provider';
 export { ThemeToggler } from './theme-toggler';
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from './dialog';
