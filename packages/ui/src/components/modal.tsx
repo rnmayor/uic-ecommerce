78cbfe918c75467c869e0396290a365426f57454
Ronel Mayor <ronelmayor0428@gmail.com>
2026-01-03 16:13:34 +0800

    feat: Created common modal in packages>ui and auth layout in admin app

---
+++ packages/ui/src/components/modal.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from './dialog';
+
+interface ModalProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  footer?: React.ReactNode;
+}
+
+export const Modal = ({ title, description, isOpen, onClose, children, footer }: ModalProps) => {
+  const onChange = () => {
+    if (!open) {
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div>{children}</div>
+        {footer && <DialogFooter>{footer}</DialogFooter>}
+      </DialogContent>
+    </Dialog>
+  );
+};
