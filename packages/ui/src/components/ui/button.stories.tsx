022a990fe5e424896e1c1397a186d45a5a3331e5
Ronel Mayor <ronelmayor0428@gmail.com>
2026-03-26 22:22:59 +0800

    fix: Added fetch-depth to 0 to Detect Changes job step to fetch full history in the changes job:

--- packages/ui/src/components/ui/button.stories.tsx
+++ packages/ui/src/components/ui/button.stories.tsx
@@ -13,35 +13,42 @@ type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
   args: {
-    children: 'Click Me',
+    children: 'Click Me...',
     variant: 'default',
   },
 };
 
 export const Secondary: Story = {
   args: {
-    children: 'Click Me',
+    children: 'Click Me...',
     variant: 'secondary',
   },
 };
 
 export const Outline: Story = {
   args: {
-    children: 'Click Me',
+    children: 'Click Me...',
     variant: 'outline',
   },
 };
 
 export const Ghost: Story = {
   args: {
-    children: 'Click Me',
+    children: 'Click Me...',
     variant: 'ghost',
   },
 };
 
 export const Destructive: Story = {
   args: {
-    children: 'Click Me',
+    children: 'Click Me...',
     variant: 'destructive',
+  },
+};
+
+export const Link: Story = {
+  args: {
+    children: 'Click Me...',
+    variant: 'link',
   },
 };
